04a4f6227cdabf223a68215ee8b0acf7c54e2df4
Your Name <you@example.com>
2022-01-18 22:21:05 +0700

    22a

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
-import './halamanlain.dart';
+
+var lokasi = [
+  {
+    'kota': 'Jakarta',
+    'img':
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Jakarta_Panorama.jpg/300px-Jakarta_Panorama.jpg'
+  },
+  {
+    'kota': 'Surabaya',
+    'img':
+        'https://www.goodnewsfromindonesia.id/uploads/post/large-surabaya-6b40c47198235b9b7d075492a0745ca8.jpg'
+  },
+  {
+    'kota': 'Jogjakarta',
+    'img': 'https://www.w3schools.com/w3css/img_lights.jpg'
+  }
+];
 
 void main() {
   runApp(const MaterialApp(
@@ -18,21 +34,18 @@ class Myapp extends StatelessWidget {
         title: const Text("Halaman Utama"),
       ),
       body: ListView(
-        children: const [
-          ListTile(
-            leading: Icon(Icons.map),
-            title: Text('Map'),
+          children: lokasi.map((nama) {
+        return Card(
+          child: Column(
+            children: [
+              Image(
+                image: NetworkImage('${nama['img']}'),
+              ),
+              Text('${nama['kota']}')
+            ],
           ),
-          ListTile(
-            leading: Icon(Icons.photo_album),
-            title: Text('Album'),
-          ),
-          ListTile(
-            leading: Icon(Icons.phone),
-            title: Text('Phone'),
-          ),
-        ],
-      ),
+        );
+      }).toList()),
     );
   }
 }
