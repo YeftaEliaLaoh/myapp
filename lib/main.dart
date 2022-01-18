1c9a4447baede409cfe42828f95f33bd352169ef
Your Name <you@example.com>
2022-01-18 22:11:35 +0700

    21

--- lib/main.dart
+++ lib/main.dart
@@ -17,56 +17,21 @@ class Myapp extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Halaman Utama"),
       ),
-      body: Container(
-        padding: const EdgeInsets.all(10.0),
-        child: ListView(
-          children: [
-            GestureDetector(
-              onTap: () {
-                Route route = MaterialPageRoute(
-                    builder: (context) => const Halamanlain());
-                Navigator.push(context, route);
-              },
-              child: Card(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Image(
-                      image: NetworkImage(
-                          "https://www.w3schools.com/w3css/img_lights.jpg"),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(10.0),
-                      child: const Text("Flutter Tutorial 01"),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                Route route = MaterialPageRoute(
-                    builder: (context) => const Halamanlain());
-                Navigator.push(context, route);
-              },
-              child: Card(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Image(
-                      image: NetworkImage(
-                          "https://www.w3schools.com/w3css/img_lights.jpg"),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(10.0),
-                      child: const Text("Flutter Tutorial 02"),
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+      body: ListView(
+        children: const [
+          ListTile(
+            leading: Icon(Icons.map),
+            title: Text('Map'),
+          ),
+          ListTile(
+            leading: Icon(Icons.photo_album),
+            title: Text('Album'),
+          ),
+          ListTile(
+            leading: Icon(Icons.phone),
+            title: Text('Phone'),
+          ),
+        ],
       ),
     );
   }
