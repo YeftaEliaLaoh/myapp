f60ef0fe15777dc1c43a55f50a24013e7886e38a
Your Name <you@example.com>
2022-01-18 16:18:54 +0700

    7

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,26 @@ class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("My Apps"),
+      appBar: AppBar(
+        title: const Text("My Apps"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(20.0),
+        child: Container(
+          width: 300.0,
+          height: 100.0,
+          child: Image.asset(
+            'image01.jpg',
+            width: 300.0,
+          ),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(
+                    'http://code.byriza.com/lib/blog/flutter225.png'),
+                fit: BoxFit.cover),
+          ),
         ),
-        body: Container(
-            padding: const EdgeInsets.all(20.0),
-            child: Container(
-                alignment: const Alignment(0, 0),
-                width: 300.0,
-                height: 100.0,
-                color: Colors.blue,
-                child: Image.asset(
-                  'image01.jpg',
-                  width: 300.0,
-                ))));
+      ),
+    );
   }
 }
