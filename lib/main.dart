ed3a0b16340895917a7bba8c241a4ebd12b7032b
Your Name <you@example.com>
2022-01-19 00:55:14 +0700

    34a

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
   runApp(const MaterialApp(
     title: "Halaman Utama",
-    home: Halamansafearea(),
+    home: Halamancarousell(),
   ));
 }
 
-class Halamansafearea extends StatelessWidget {
-  const Halamansafearea({Key? key}) : super(key: key);
+class Halamancarousell extends StatelessWidget {
+  const Halamancarousell({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Halaman SafeArea"),
+          title: const Text("Carousel"),
         ),
-        body: SafeArea(
-          bottom: true,
-          top: true,
-          right: true,
-          left: true,
-          child: Container(
-            padding: const EdgeInsets.all(10.0),
-            child: ListView(
-                children:
-                    List.generate(50, (i) => Text("This is Text number $i"))),
-          ),
+        body: CarouselSlider(
+          options: CarouselOptions(height: 400.0),
+          items: [1, 2, 3, 4, 5].map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                    decoration: const BoxDecoration(color: Colors.amber),
+                    child: Text(
+                      'text $i',
+                      style: const TextStyle(fontSize: 16.0),
+                    ));
+              },
+            );
+          }).toList(),
         ));
   }
 }
