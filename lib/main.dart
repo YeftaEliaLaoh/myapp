a31bb31ffd4cd8f4f9b9b88b3074ce771f4a25bc
Your Name <you@example.com>
2022-01-18 16:48:38 +0700

    12

--- lib/main.dart
+++ lib/main.dart
@@ -17,18 +17,150 @@ class Myapp extends StatelessWidget {
         title: const Text("My Apps"),
       ),
       body: ListView(
-        children: const [
-          ListTile(
-            leading: Icon(Icons.map),
-            title: Text('Map'),
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.blue,
+                  child: const Icon(
+                    Icons.home,
+                    color: Colors.white,
+                    size: 50.0,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(15.0),
+                    height: 100.0,
+                    color: Colors.blue[200],
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text("Artikel 1",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 15.0)),
+                        Text(
+                          "Ini adalah contoh artikel pada listview dengan versi custom",
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
-          ListTile(
-            leading: Icon(Icons.photo_album),
-            title: Text('Album'),
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.blue,
+                  child: const Icon(
+                    Icons.home,
+                    color: Colors.white,
+                    size: 50.0,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(15.0),
+                    height: 100.0,
+                    color: Colors.blue[200],
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text("Artikel 2",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 15.0)),
+                        Text(
+                          "Ini adalah contoh artikel pada listview dengan versi custom",
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
-          ListTile(
-            leading: Icon(Icons.phone),
-            title: Text('Phone'),
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.blue,
+                  child: const Icon(
+                    Icons.home,
+                    color: Colors.white,
+                    size: 50.0,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(15.0),
+                    height: 100.0,
+                    color: Colors.blue[200],
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text("Artikel 3",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 15.0)),
+                        Text(
+                          "Ini adalah contoh artikel pada listview dengan versi custom",
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.blue,
+                  child: const Icon(
+                    Icons.home,
+                    color: Colors.white,
+                    size: 50.0,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(15.0),
+                    height: 100.0,
+                    color: Colors.blue[200],
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text("Artikel 4",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 15.0)),
+                        Text(
+                          "Ini adalah contoh artikel pada listview dengan versi custom",
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ],
       ),
