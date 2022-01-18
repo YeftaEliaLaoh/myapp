ec78ae9ce7c1e2a99033e0f84bfa1ce46d5bbded
Your Name <you@example.com>
2022-01-18 16:51:14 +0700

    13

--- lib/main.dart
+++ lib/main.dart
@@ -16,153 +16,58 @@ class Myapp extends StatelessWidget {
       appBar: AppBar(
         title: const Text("My Apps"),
       ),
-      body: ListView(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              children: [
-                Container(
-                  width: 100.0,
-                  height: 100.0,
-                  color: Colors.blue,
-                  child: const Icon(
-                    Icons.home,
-                    color: Colors.white,
-                    size: 50.0,
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.all(15.0),
-                    height: 100.0,
-                    color: Colors.blue[200],
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text("Artikel 1",
-                            style:
-                                TextStyle(color: Colors.white, fontSize: 15.0)),
-                        Text(
-                          "Ini adalah contoh artikel pada listview dengan versi custom",
-                          style: TextStyle(color: Colors.white),
-                        )
-                      ],
-                    ),
-                  ),
-                )
-              ],
+      body: SizedBox(
+        height: 100.0,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.blue,
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              children: [
-                Container(
-                  width: 100.0,
-                  height: 100.0,
-                  color: Colors.blue,
-                  child: const Icon(
-                    Icons.home,
-                    color: Colors.white,
-                    size: 50.0,
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.all(15.0),
-                    height: 100.0,
-                    color: Colors.blue[200],
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text("Artikel 2",
-                            style:
-                                TextStyle(color: Colors.white, fontSize: 15.0)),
-                        Text(
-                          "Ini adalah contoh artikel pada listview dengan versi custom",
-                          style: TextStyle(color: Colors.white),
-                        )
-                      ],
-                    ),
-                  ),
-                )
-              ],
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.green,
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              children: [
-                Container(
-                  width: 100.0,
-                  height: 100.0,
-                  color: Colors.blue,
-                  child: const Icon(
-                    Icons.home,
-                    color: Colors.white,
-                    size: 50.0,
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.all(15.0),
-                    height: 100.0,
-                    color: Colors.blue[200],
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text("Artikel 3",
-                            style:
-                                TextStyle(color: Colors.white, fontSize: 15.0)),
-                        Text(
-                          "Ini adalah contoh artikel pada listview dengan versi custom",
-                          style: TextStyle(color: Colors.white),
-                        )
-                      ],
-                    ),
-                  ),
-                )
-              ],
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.yellow,
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              children: [
-                Container(
-                  width: 100.0,
-                  height: 100.0,
-                  color: Colors.blue,
-                  child: const Icon(
-                    Icons.home,
-                    color: Colors.white,
-                    size: 50.0,
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.all(15.0),
-                    height: 100.0,
-                    color: Colors.blue[200],
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text("Artikel 4",
-                            style:
-                                TextStyle(color: Colors.white, fontSize: 15.0)),
-                        Text(
-                          "Ini adalah contoh artikel pada listview dengan versi custom",
-                          style: TextStyle(color: Colors.white),
-                        )
-                      ],
-                    ),
-                  ),
-                )
-              ],
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.blue,
             ),
-          ),
-        ],
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.green,
+            ),
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.yellow,
+            ),
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.green,
+            ),
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.yellow,
+            ),
+          ],
+        ),
       ),
     );
   }
