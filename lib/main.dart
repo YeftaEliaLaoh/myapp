7f695d68beb2663bc0b8e5021f7a67e9250cc4b7
Your Name <you@example.com>
2022-01-19 01:20:27 +0700

    37

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,34 @@
 // ignore_for_file: import_of_legacy_library_into_null_safe
 
 import 'package:flutter/material.dart';
-import 'package:koukicons/home.dart';
-import 'package:koukicons/gift.dart';
-import 'package:koukicons/pin.dart';
+import 'package:badges/badges.dart';
 
 void main() {
   runApp(const MaterialApp(
     title: "Halaman Utama",
-    home: Halamancarousell(),
+    home: Halamanhalaman(),
   ));
 }
 
-class Halamancarousell extends StatelessWidget {
-  const Halamancarousell({Key? key}) : super(key: key);
+class Halamanhalaman extends StatelessWidget {
+  const Halamanhalaman({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Carousel"),
+        title: const Text("Halaman"),
       ),
-      body: Column(
-        children: [
-          KoukiconsHome(),
-          KoukiconsGift(height: 100.0),
-          KoukiconsPin(width: 70.0, color: Colors.red),
-        ],
+      body: Badge(
+        badgeColor: Colors.deepPurple,
+        shape: BadgeShape.square,
+        borderRadius: BorderRadius.circular(10),
+        toAnimate: false,
+        badgeContent: const Text(
+          "OPEN",
+          style: TextStyle(
+              fontSize: 10.0, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
       ),
     );
   }
