11437d56ffe9168a04902f769bbdd11ed0a6ddc1
Your Name <you@example.com>
2022-01-18 22:28:42 +0700

    23

--- lib/main.dart
+++ lib/main.dart
@@ -31,22 +31,40 @@ class Myapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Halaman Utama"),
+        title: const Text("Drawer"),
       ),
-      body: ListView.builder(
-        itemCount: lokasi.length,
-        itemBuilder: (context, i) {
-          return Card(
-            child: Column(
-              children: [
-                Image(
-                  image: NetworkImage('${lokasi[i]['img']}'),
-                ),
-                Text('${lokasi[i]['kota']}')
-              ],
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const UserAccountsDrawerHeader(
+              accountName: Text("Tes"),
+              accountEmail: Text("tes@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Jakarta_Panorama.jpg/300px-Jakarta_Panorama.jpg"),
+              ),
             ),
-          );
-        },
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Item 1'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              trailing: const Icon(Icons.chevron_right),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('Item 3'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+      body: const Center(
+        child: Text("Drawer"),
       ),
     );
   }
