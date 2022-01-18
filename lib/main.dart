0e156fad15dab40ee4d44e62945fb565ece3a06d
Your Name <you@example.com>
2022-01-18 15:32:32 +0700

    4

--- lib/main.dart
+++ lib/main.dart
@@ -19,12 +19,10 @@ class Myapp extends StatelessWidget {
         body: Container(
             padding: const EdgeInsets.all(20.0),
             child: const Center(
-              child: Text(
-                "Hello World",
-                style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold),
+              child: Icon(
+                Icons.home,
+                size: 40.0,
+                color: Colors.blue,
               ),
             )));
   }
