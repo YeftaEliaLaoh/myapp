f387b4e3b17393c8a6f685069ae6c09d6ea8f0f0
Your Name <you@example.com>
2022-01-19 13:25:20 +0700

    47

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expandable/expandable.dart';
 
 void main() => runApp(const MyApp());
 
@@ -17,86 +18,23 @@ class _MyAppState extends State {
         appBar: AppBar(
           title: const Text('Border example app'),
         ),
-        body: Center(
-          child: GridView.count(
-            crossAxisCount: 3,
-            children: [
-              Container(
-                width: 100.0,
-                height: 100.0,
-                margin: const EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  border: Border.all(color: Theme.of(context).primaryColor),
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-              ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                margin: const EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  border: Border.all(color: Theme.of(context).primaryColor),
-                  borderRadius: const BorderRadius.all(Radius.circular(20.0)),
-                ),
-              ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                margin: const EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  border: Border.all(color: Theme.of(context).primaryColor),
-                  borderRadius:
-                      const BorderRadius.all(Radius.elliptical(10.0, 20.0)),
-                ),
-              ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                margin: const EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  color: Colors.yellow,
-                  border: Border.all(color: Theme.of(context).primaryColor),
-                  borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(20.0)),
-                ),
-              ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                margin: const EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  color: Colors.teal,
-                  border: Border.all(color: Theme.of(context).primaryColor),
-                  borderRadius: const BorderRadius.only(
-                      bottomRight: Radius.circular(20.0)),
-                ),
-              ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                margin: const EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  color: Colors.amber,
-                  border: Border.all(color: Theme.of(context).primaryColor),
-                  borderRadius:
-                      const BorderRadius.only(topLeft: Radius.circular(20.0)),
-                ),
-              ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                margin: const EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  color: Colors.brown,
-                  border: Border.all(color: Theme.of(context).primaryColor),
-                  borderRadius:
-                      const BorderRadius.only(topRight: Radius.circular(20.0)),
-                ),
-              ),
-            ],
+        body: Container(
+          padding: const EdgeInsets.all(10.0),
+          child: ExpandablePanel(
+            header: const Text(
+              "Contoh by Test",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+            ),
+            collapsed: const Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse augue arcu,',
+              softWrap: true,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            expanded: const Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse augue arcu, dapibus vel porta id, ultrices quis leo. Cras id quam quis neque mattis viverra ac eget ante. Etiam fringilla eget enim eget lacinia.',
+              softWrap: true,
+            ),
           ),
         ),
       ),
