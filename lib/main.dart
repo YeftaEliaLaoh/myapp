b6883ee0d13da9baf87a3a9d2621756b60788f6f
Your Name <you@example.com>
2022-01-19 13:19:53 +0700

    46

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:share_plus/share_plus.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,29 +10,96 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State {
-  void share(BuildContext context) {
-    var url = "https://www.ets.org/toefl";
-    RenderBox box = context.findRenderObject() as RenderBox;
-
-    Share.share(url,
-        subject: "Website",
-        sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Plugin example app'),
+        appBar: AppBar(
+          title: const Text('Border example app'),
+        ),
+        body: Center(
+          child: GridView.count(
+            crossAxisCount: 3,
+            children: [
+              Container(
+                width: 100.0,
+                height: 100.0,
+                margin: const EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  border: Border.all(color: Theme.of(context).primaryColor),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                margin: const EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  border: Border.all(color: Theme.of(context).primaryColor),
+                  borderRadius: const BorderRadius.all(Radius.circular(20.0)),
+                ),
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                margin: const EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  border: Border.all(color: Theme.of(context).primaryColor),
+                  borderRadius:
+                      const BorderRadius.all(Radius.elliptical(10.0, 20.0)),
+                ),
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                margin: const EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  border: Border.all(color: Theme.of(context).primaryColor),
+                  borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(20.0)),
+                ),
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                margin: const EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: Colors.teal,
+                  border: Border.all(color: Theme.of(context).primaryColor),
+                  borderRadius: const BorderRadius.only(
+                      bottomRight: Radius.circular(20.0)),
+                ),
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                margin: const EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  border: Border.all(color: Theme.of(context).primaryColor),
+                  borderRadius:
+                      const BorderRadius.only(topLeft: Radius.circular(20.0)),
+                ),
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                margin: const EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: Colors.brown,
+                  border: Border.all(color: Theme.of(context).primaryColor),
+                  borderRadius:
+                      const BorderRadius.only(topRight: Radius.circular(20.0)),
+                ),
+              ),
+            ],
           ),
-          body: Center(
-              child: ElevatedButton(
-            child: const Text("Share"),
-            onPressed: () {
-              share(context);
-            },
-          ))),
+        ),
+      ),
     );
   }
 }
