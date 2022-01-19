fb090e57f2598ac74bdfd60154c85d3e62d86fae
Your Name <you@example.com>
2022-01-19 22:12:22 +0700

    81

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,10 @@
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:myapp/views/datepicker.dart';
+import 'package:myapp/homescreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    localizationsDelegates: [
-      GlobalMaterialLocalizations.delegate,
-      GlobalWidgetsLocalizations.delegate,
-      GlobalCupertinoLocalizations.delegate,
-    ],
-    supportedLocales: [
-      Locale('id', 'ID'),
-    ],
     debugShowCheckedModeBanner: false,
     title: "My App",
-    home: Halaman(),
+    home: HomeScreen(),
   ));
 }
