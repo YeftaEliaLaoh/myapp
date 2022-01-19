284e687aa29c2f75d68e51290446a5d91c73b4bc
Your Name <you@example.com>
2022-01-19 11:34:17 +0700

    41

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,74 @@
-import 'dart:convert';
-import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    title: "My Apps",
-    home: Halaman(),
-  ));
-}
+void main() => runApp(const MyApp());
 
-class Halaman extends StatelessWidget {
-  const Halaman({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Sample App';
+
   @override
   Widget build(BuildContext context) {
-    var tgl = DateTime.now();
-    var bytes = utf8.encode(tgl.toString());
-    var hashed = md5.convert(bytes);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("DateTime Flutter"),
-      ),
-      body: Center(
-        child: Text("Tanggal : " + hashed.toString()),
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const MyStatefulWidget(),
       ),
     );
   }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+                height: 50,
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: ElevatedButton(
+                  child: const Text('Simpan'),
+                  onPressed: () {
+                    _simpan();
+                  },
+                )),
+            Container(
+                height: 50,
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: ElevatedButton(
+                  child: const Text('panggil'),
+                  onPressed: () {
+                    _panggil();
+                  },
+                )),
+          ],
+        ));
+  }
+}
+
+_simpan() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var _text = "String yang disimpan";
+  await prefs.setString('text_tersimpan', _text);
+}
+
+_panggil() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var _ambiltext = prefs.getString('text_tersimpan');
+  // ignore: avoid_print
+  print(_ambiltext);
 }
