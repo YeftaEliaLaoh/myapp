76a998adcea5e0f5fe7a4932e58253557f9cd928
Your Name <you@example.com>
2022-01-19 21:44:02 +0700

    78

--- lib/views/datepicker.dart
+++ lib/views/datepicker.dart
@@ -5,13 +5,14 @@ class Halaman extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String kalimat = "Belajar Mobile-App Flutter hari ini pukul 08:10";
-    var hasil = kalimat.split(' ');
+    String kalimat =
+        "Belajar Mobile-App Flutter hari ini pukul 08:10".replaceAll('e', 'x');
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("DateTime Flutter"),
       ),
-      body: Center(child: Text("hasil : " + hasil[6])),
+      body: Center(child: Text("hasil : " + kalimat)),
     );
   }
 }
