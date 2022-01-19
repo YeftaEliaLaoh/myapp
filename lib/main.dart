1dda71806830515ea1dd5b9822670e480c81b62a
Your Name <you@example.com>
2022-01-19 13:48:12 +0700

    53

--- lib/main.dart
+++ lib/main.dart
@@ -2,38 +2,54 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    title: "Family Dex",
-    home: TabBarDemo(),
+    title: "Halaman",
+    home: Halaman(),
   ));
 }
 
-class TabBarDemo extends StatelessWidget {
-  const TabBarDemo({Key? key}) : super(key: key);
+class Halaman extends StatefulWidget {
+  const Halaman({Key? key}) : super(key: key);
+
+  @override
+  _HalamanState createState() => _HalamanState();
+}
+
+class _HalamanState extends State with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: 3);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: const TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.access_time), text: 'Tab 1'),
-                Tab(icon: Icon(Icons.access_alarms), text: 'Tab 2'),
-                Tab(icon: Icon(Icons.account_circle), text: 'Tab 3'),
-              ],
-            ),
-            title: const Text('Tabbar'),
-          ),
-          body: const TabBarView(
-            children: [
-              Center(child: Text('Ini Tab 1')),
-              Center(child: Text('Ini Tab 2')),
-              Center(child: Text('Ini Tab 3')),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(icon: Icon(Icons.access_time), text: 'Tab 1'),
+            Tab(icon: Icon(Icons.access_alarms), text: 'Tab 2'),
+            Tab(icon: Icon(Icons.account_circle), text: 'Tab 3'),
+          ],
         ),
+        title: const Text('Tabbar'),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: const [
+          Center(child: Text('Ini Tab 1')),
+          Center(child: Text('Ini Tab 2')),
+          Center(child: Text('Ini Tab 3')),
+        ],
       ),
     );
   }
