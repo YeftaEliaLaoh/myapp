f9aaef261d258e60fde569353e8a2e24916e428d
Your Name <you@example.com>
2022-01-19 10:21:45 +0700

    40

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,28 @@
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    title: "Halaman",
+    title: "My Apps",
     home: Halaman(),
   ));
 }
 
 class Halaman extends StatelessWidget {
   const Halaman({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    var tgl = DateTime.now();
+    var bytes = utf8.encode(tgl.toString());
+    var hashed = md5.convert(bytes);
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Show Dialog"),
-        ),
-        body: Center(
-            child: ElevatedButton(
-                onPressed: () {
-                  _showSimpleDialog(context);
-                },
-                child: const Text("Click Here"))));
-  }
-
-  void _showSimpleDialog(context) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return SimpleDialog(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-              child: Row(
-                children: [
-                  const Expanded(
-                    child: Text(
-                      "Tambah Pengguna",
-                      style: TextStyle(fontSize: 20.0),
-                    ),
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.close),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  )
-                ],
-              ),
-            ),
-            const Divider(),
-            const Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Text(
-                "Nama Lengkap",
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-              child: TextField(
-                style: const TextStyle(
-                    fontSize: 14.0, height: 1.0, color: Colors.black),
-                decoration: InputDecoration(
-                  hintText: "Input nama lengkap",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(3.0)),
-                  contentPadding: const EdgeInsets.symmetric(
-                      vertical: 14.0, horizontal: 15.0),
-                ),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Text(
-                "Kontak",
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-              child: TextField(
-                style: const TextStyle(
-                    fontSize: 14.0, height: 1.0, color: Colors.black),
-                decoration: InputDecoration(
-                  hintText: "Input kontak",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(3.0)),
-                  contentPadding: const EdgeInsets.symmetric(
-                      vertical: 14.0, horizontal: 15.0),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0),
-              child: ElevatedButton(
-                child: const Text(
-                  "SIMPAN",
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () {},
-              ),
-            )
-          ],
-        );
-      },
+      appBar: AppBar(
+        title: const Text("DateTime Flutter"),
+      ),
+      body: Center(
+        child: Text("Tanggal : " + hashed.toString()),
+      ),
     );
   }
 }
