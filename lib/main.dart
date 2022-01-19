bd345c992740789076510b29b98f125c18ff1925
Your Name <you@example.com>
2022-01-19 13:42:11 +0700

    51a

--- lib/main.dart
+++ lib/main.dart
@@ -13,37 +13,19 @@ class Halaman extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ProgressDialog pr =
-        ProgressDialog(context, type: ProgressDialogType.Normal);
-
-    pr.style(
-      message: 'Menunggu...',
-      borderRadius: 10.0,
-      backgroundColor: Colors.white,
-      elevation: 10.0,
-      insetAnimCurve: Curves.easeInOut,
-      progress: 0.0,
-      maxProgress: 100.0,
-      progressTextStyle: const TextStyle(
-          color: Colors.black, fontSize: 13.0, fontWeight: FontWeight.w400),
-      messageTextStyle: const TextStyle(
-          color: Colors.black, fontSize: 19.0, fontWeight: FontWeight.w600),
-    );
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("Progress Dialog"),
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text("Tampilkan"),
-          onPressed: () {
-            pr.show();
-
-            Future.delayed(const Duration(seconds: 10)).then((onValue) {
-              pr.hide();
-            });
-          },
+      body: Container(
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30.0),
+          image: const DecorationImage(
+            image: AssetImage('image01.jpg'),
+            fit: BoxFit.cover,
+          ),
         ),
       ),
     );
