b97eb03a07a94dd7141a28b32206a1c6d4057619
Your Name <you@example.com>
2022-01-19 11:41:36 +0700

    42

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MaterialApp(
+    title: "My Carousell",
+    home: MyApp(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+  @override
+  _ContohappbarState createState() => _ContohappbarState();
+}
 
-  static const String _title = 'Sample App';
-
+class _ContohappbarState extends State {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: const MyStatefulWidget(),
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.green,
+          elevation: 0.0,
+          iconTheme: const IconThemeData(
+            color: Colors.lightBlue, //change your color here
+          ),
+          leading: IconButton(
+            icon: const Icon(Icons.supervised_user_circle),
+            onPressed: () {},
+          ),
+          title: const Text("Ini AppBar"),
+          actions: [
+            // action button
+            IconButton(
+              icon: const Icon(Icons.add_circle),
+              onPressed: () {
+                //action
+              },
+            ),
+            // action button
+            IconButton(
+              icon: const Icon(Icons.add_location),
+              onPressed: () {
+                //action
+              },
+            ),
+          ]),
+      body: const Center(
+        child: Text("ini body"),
       ),
     );
   }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: ElevatedButton(
-                  child: const Text('Simpan'),
-                  onPressed: () {
-                    _simpan();
-                  },
-                )),
-            Container(
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: ElevatedButton(
-                  child: const Text('panggil'),
-                  onPressed: () {
-                    _panggil();
-                  },
-                )),
-          ],
-        ));
-  }
-}
-
-_simpan() async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  var _text = "String yang disimpan";
-  await prefs.setString('text_tersimpan', _text);
-}
-
-_panggil() async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  var _ambiltext = prefs.getString('text_tersimpan');
-  // ignore: avoid_print
-  print(_ambiltext);
 }
