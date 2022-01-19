8c0e37443ec3ae037662c8cb734804081664001e
Your Name <you@example.com>
2022-01-19 21:06:27 +0700

    71

--- lib/tabbarbody.dart
+++ lib/tabbarbody.dart
@@ -14,29 +14,70 @@ class _TabbarBodyState extends State {
       appBar: AppBar(
         title: const Text("Tabbar Body"),
       ),
-      body: DefaultTabController(
-        length: 3,
+      body: SingleChildScrollView(
         child: Column(
-          children: const [
-            TabBar(
-              labelColor: Colors.black,
-              tabs: [
-                Tab(text: 'Tab 1'),
-                Tab(text: 'Tab 2'),
-                Tab(text: 'Tab 3'),
-              ],
-            ),
-            Expanded(
-              child: TabBarView(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
                 children: [
-                  Center(
-                    child: Text("Tab 1"),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100.0),
+                      image: const DecorationImage(
+                        image: NetworkImage(
+                            'https://www.byriza.com/lib/blog/1586938494.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
-                  Center(
-                    child: Text("Tab 2"),
+                  const SizedBox(
+                    width: 20.0,
                   ),
-                  Center(
-                    child: Text("Tab 3"),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text("Username"),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text("follow"),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            DefaultTabController(
+              length: 3,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const TabBar(
+                    labelColor: Colors.black,
+                    tabs: [
+                      Tab(text: 'Tab 1'),
+                      Tab(text: 'Tab 2'),
+                      Tab(text: 'Tab 3'),
+                    ],
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height,
+                    child: const TabBarView(
+                      children: [
+                        Center(
+                          child: Text("Tab 1"),
+                        ),
+                        Center(
+                          child: Text("Tab 2"),
+                        ),
+                        Center(
+                          child: Text("Tab 3"),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
