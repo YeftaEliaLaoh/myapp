48d37c5fdf65e2285180ba9d461de53732420572
Your Name <you@example.com>
2022-01-19 22:26:39 +0700

    85

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,28 @@
-import 'package:myapp/customslideupmenu.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: "My App",
-    home: CustomSlideUpMenu(),
+    title: "My Apps",
+    home: Halaman(),
   ));
+}
+
+class Halaman extends StatelessWidget {
+  const Halaman({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final nominal = NumberFormat("#,##0.00", "en_US");
+    int uang = 2300000;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("nominal Flutter"),
+      ),
+      body: Center(
+        child: Text("nominal : " + nominal.format(uang)),
+      ),
+    );
+  }
 }
